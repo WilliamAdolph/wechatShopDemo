644b9c560182fea52368485cd1eab59b4f94bca5
WilliamAdolph <1208479837@qq.com>
2020-08-10 23:59:53 +0800

    login修改

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,7 +5,40 @@ Page({
    * 页面的初始数据
    */
   data: {
+    phone_number: '',
+    code: '',
+  },
 
+  getPhone(e) {
+    if(!(/^1[3456789]\d{9}$/.test(e.detail.value))) {
+      wx.showToast ({
+        title: '手机号输入不正确',
+        icon: 'none',
+        duration: 1000
+      })
+      setTimeout(()=> { wx.hideToast(); },1000)
+    } else {
+      this.setData({
+        phone_number: e.detail.value
+      })
+    }
+  },
+  getCode(e) {
+    this.setData({
+      code: e.detail.value
+    })
+  },
+  login() {
+    wx.request({
+      url: 'http://www.hengyishun.cn/login/login',
+      data: ({
+        phone: this.data.phone_number,
+        code: this.data.code
+      }),
+      success(res) {
+        console.log(res,'登录成功');
+      }
+    })
   },
 
   /**
